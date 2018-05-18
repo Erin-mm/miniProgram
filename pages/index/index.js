9edee879113d3bab56ad4198dc01810138b6494a
yuanmj <yuanmj@Ctrip.com>
2018-05-18 15:05:08 +0800

    second commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,15 +8,40 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imgSrc: ''
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  clickMe:function(){
+  goToStatistics:function(){
+    wx.navigateTo({
+      url: '../statistics/statistics'
+    })
+  },
+  goToNotes: function () {
+    wx.navigateTo({
+      url: '../notes/notes'
+    })
+  },
+  clickMe: function () {
     this.setData({ motto: "Hello Erin" })
+  },
+  chooseImg: function () {
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+        setImg(tempFilePaths)
+      }
+    })
+  },
+  setImg: function (val) {
+    console.log(val)
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -24,7 +49,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -46,7 +71,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
---
+++ pages/notes/notes.js
@@ -0,0 +1,74 @@
+// pages/notes/notes.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgArr: [
+      { name: 'happy', url: '../../sources/images/happy.png' },
+      { name: 'hehe', url: '../../sources/images/hehe.png' },
+      { name: 'kiss', url: '../../sources/images/kiss.png' },
+      { name: 'pride', url: '../../sources/images/pride.png' },
+      { name: 'sad', url: '../../sources/images/sad.png' },
+      { name: 'shocked', url: '../../sources/images/shocked.png' },
+      { name: 'smile', url: '../../sources/images/smile.png' },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
