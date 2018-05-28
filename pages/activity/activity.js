68bf88e7bd0b2dcc9a7222a1a5bef00d93bfc198
yuanmj <yuanmj@Ctrip.com>
2018-05-28 18:24:30 +0800

    calendar

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -1,4 +1,6 @@
 // pages/activity/activity.js
+const util = require('../../utils/util.js')
+
 Page({
 
   /**
@@ -6,7 +8,7 @@ Page({
    */
   data: {
     date: [],
-    weeks: [],
+    today: util.formatDate(new Date()),
     scrollTop: ''
   },
 
@@ -14,8 +16,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.dateData(2017, 2019);
-    this.getFields();
+    var that = this;
+    that.dateData(2017, 2019);
+    that.getFields();
   },
 
   /**
@@ -65,87 +68,21 @@ Page({
 
   },
   dateData: function (minY, maxY) {
-    let dataAll = []//总日历数据
-    let dataAll2 = []//总日历数据
-    let dataMonth = []//月日历数据
-    let date = new Date//当前日期
-    let year = date.getFullYear()//当前年
-    let week = date.getDay();//当天星期几
-    let weeks = []
-    let month = date.getMonth() + 1//当前月份
-    let day = date.getDate()//当天
-    let daysCount = 0//一共显示多少天
-    let maxYear = maxY//最大显示年份
-    let minYear = minY//最小显示年份
-    let dayscNow = 0//计数器
-    let monthList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]//月份列表
-    let nowMonthList = []//本年剩余年份
-    for (let i = month; i < 13; i++) {
-      nowMonthList.push(i)
-    }
-    let yearList = []
-    for (let i = 0; i <= maxYear - minYear; i++) {
-      let nowYear = minYear
-      nowYear += i
-      yearList.push(nowYear)
-    }
+    const dataAll = [];
 
-
-    let leapYear = function (Year) {//判断是否闰年 
-      if (((Year % 4) == 0) && ((Year % 100) != 0) || ((Year % 400) == 0)) {
-        return (true);
-      } else { return (false); }
-    }
-
-    for (let i = 0; i <= yearList.length; i++) {
-      daysCount += 365 + leapYear(yearList[i])
-    }
-    for (let i = 0; i < yearList.length; i++) {//遍历年
-      let mList = monthList
-      for (let j = 0; j < mList.length; j++) {//循环月份
-        dataMonth = []
-        let t_days = [31, 28 + leapYear(yearList[i]), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        for (let k = 0; k < t_days[j]; k++) {//循环每天
-          dayscNow++
-          let nowData
-          if (dayscNow < daysCount) {//如果计数器没满
-            let days = k + 1
-            if (days < 10) {
-              days = "0" + days
-            }
-            nowData = {//组装自己需要的数据
-              year: yearList[i],
-              month: mList[j],
-              day: k + 1,
-              date: yearList[i] + "" + mList[j] + days,
-              re: yearList[i] + "-" + mList[j] + "-" + days,
-            }
-            if (yearList[i] == year && mList[j] == month && k + 1 == day) {
-              nowData.selected = 1
-            } else {
-              nowData.selected = 0
-            }
-            dataMonth.push(nowData)
-            if (k == 0) {
-              let date = new Date(yearList[i] + "-" + mList[j] + "-" + k + 1)
-              let weekss = date.getDay()//获取每个月第一天是周几
-              weeks.push(weekss)
-            }
-          } else {
-            break
-          }
-        }
-        dataAll.push(dataMonth)
+    for (let i = minY; i <= maxY; i++) {
+      for (let j = 1; j <= 12; j++) {
+        const newDate = new Date(i, j, 0);
+        dataAll.push({
+          year: i,
+          month: j,
+          data: util.createMonth(newDate)
+        });
       }
     }
-    for (let i = 0; i < dataAll.length; i++) {
-      if (dataAll[i].length != 0) {
-        dataAll2.push(dataAll[i]);
-      }
-    }
+    console.log('dataAll', dataAll)
     this.setData({
-      date: dataAll2,
-      weeks: weeks
+      date: dataAll
     })
   },
   selectDay: function () {
@@ -154,7 +91,9 @@ Page({
 
   getFields: function () {
     var that = this
-    wx.createSelectorQuery().select('.current').boundingClientRect(function (res) {
+    var current = wx.createSelectorQuery().select('.current');
+    console.log(current);
+    current._component && current.boundingClientRect(function (res) {
       var scrollTop = res.top - (Math.floor((res.dataset.indexs + res.dataset.week) / 7) * 53 + 44 + 27);
       that.data.scrollTop = scrollTop;
       that.scrollTop()
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,5 @@
+const weekStr = ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -14,6 +16,111 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const isLeapYear = year => {
+  return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)
+}
+
+const getMonthDays = (year, month) => {
+  return [31, null, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month] || (isLeapYear(year) ? 29 : 28)
+}
+
+const getWeekNumber = (date) => {
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  let days = date.getDate()
+
+  //那一天是那一年中的第多少天
+  for (var i = 0; i < month; i++) {
+    days += getMonthDays(year, i)
+  }
+
+  //那一年第一天是星期几
+  var yearFirstDay = new Date(year, 0, 1).getDay() || 7
+
+  var week = null
+  if (yearFirstDay == 1) {
+    week = Math.ceil(days / 7) // Math.ceil(days / yearFirstDay)
+  } else {
+    days -= (7 - yearFirstDay + 1)
+    week = Math.ceil(days / 7) + 1
+  }
+
+  return week
+}
+
+const pad = (val, len) => {
+  val = val + ''
+  len = len || 2
+  while (val.length < len) {
+    val = '0' + val
+  }
+  return val
+}
+
+const formatDate = (date, mask) => {
+  mask = mask || 'yyyy-mm-dd'
+  const token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|"[^"]*"|'[^']*'/g
+  const d = date.getDate(),
+    m = date.getMonth(),
+    y = date.getFullYear(),
+    H = date.getHours(),
+    _M = date.getMinutes(),
+    s = date.getSeconds(),
+    L = date.getTime(),
+
+    flags = {
+      d: d,
+      dd: pad(d),
+      m: m + 1,
+      mm: pad(m + 1),
+      yy: String(y).slice(2),
+      yyyy: y,
+      H: H,
+      M: _M,
+      S: s,
+      L: pad(L, 3)
+    }
+
+  return mask.replace(token, function ($0) {
+    return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1)
+  })
+
+}
+const createMonth = date => {
+  const _y = date.getFullYear()
+  const _m = date.getMonth()
+  const min = new Date(_y, _m, 0).getDay()
+  const month = []
+
+  for (let i = 0; i < 42; i++) {
+    let d_item = new Date(_y, _m, (i - min))
+
+    let _dstr = formatDate(d_item)
+    let obj = {
+      o: d_item,
+      k: _dstr,
+      i: parseInt(_dstr.replace(/-/gi, ''), 10),
+      y: d_item.getFullYear(),
+      m: d_item.getMonth() + 1,
+      d: d_item.getDate(),
+      w: d_item.getDay(),
+      th: getWeekNumber(d_item),
+      ww: weekStr[d_item.getDay()]
+    }
+
+    month.push(obj)
+  }
+
+  return month
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime,
+  formatNumber,
+  isLeapYear,
+  getMonthDays,
+  getWeekNumber,
+  pad,
+  formatDate,
+  createMonth,
 }
